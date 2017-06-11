83f4979e0a7128e877407de5ad6c3fefaf13929a
Anmol Singh Sethi <anmolsinghsethi@yahoo.com>
2017-06-11 14:45:26 +0530

    Create assignment1_question2.java

---
+++ LabAssignment1/Question2/assignment1_question2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class assignment1_question2{
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		System.out.print("Enter the details of student 1: ");
+		student1.GetData();
+		student1.SetData();
+
+		Student student2 = new Student();
+		System.out.print("Enter the details of student 2: ");
+		student2.GetData();
+		student2.SetData();
+	}
+}
